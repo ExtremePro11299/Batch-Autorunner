58792a58d3ebb6f78b84fa8c89f236caba0e11a8
ExtremePro11299 <134209046+ExtremePro11299@users.noreply.github.com>
2023-08-16 16:29:31 +0200

    Add support for all users

--- Setup.cmd
+++ Setup.cmd
@@ -1,4 +1,6 @@
 @echo off
+cd /d "%~dp0"
+:checkAutorun
 if not exist Autorun.cmd (
 echo Autorun.cmd does not exist.
 echo The program may already have been set up.
@@ -8,5 +10,22 @@ exit
 )
 echo This will make your program always run when you log in. Are you sure?
 echo You can disable the runner in the Startup section of the Task Manager (ctrl+shift+esc to open)
-pause
-move "Autorun.cmd" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+echo What do you want to do?
+echo 1) Set up Autorun
+echo 2) Test Autorun
+echo 3) Exit
+set /p binput=
+if %binput%==1 goto setupp
+if %binput%==2 call Autorun.cmd
+if %binput%==3 exit
+:setupp
+echo This will make your program always run when you log in. Are you sure?
+echo.
+echo You can disable the runner in the Startup section of the Task Manager (ctrl+shift+esc to open)
+echo For who should the autorun apply?
+echo 1) Current user only
+echo 2) All users (Admin)
+set /p sinput=
+if %sinput%==1 move "Autorun.cmd" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+if %sinput%==2 move "Autorun.cmd" "C:\ProgramData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+goto checkAutorun
