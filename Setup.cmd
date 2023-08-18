fab717e0c1e413c1bd3ca561e4c574bcf11a684a
ExtremePro11299 <134209046+ExtremePro11299@users.noreply.github.com>
2023-08-18 11:00:54 +0200

    Update Setup.cmd

--- Setup.cmd
+++ Setup.cmd
@@ -8,6 +8,7 @@ echo If you want to use this for another program, reinstall Autorun.cmd from Git
 pause
 exit
 )
+:start
 echo What do you want to do?
 echo 1) Activate Autorun
 echo 2) Test Autorun
@@ -16,13 +17,14 @@ set /p binput=
 if %binput%==1 goto setupp
 if %binput%==2 call Autorun.cmd
 if %binput%==3 exit
+goto start
 :setupp
 echo This will make your program always run when you log in. Are you sure?
 echo.
 echo You can later always disable the runner in the Startup section of the Task Manager (ctrl+shift+esc to open)
 echo For who should the autorun apply?
 echo 1) Current user only
-echo 2) All users (Admin)
+echo 2) All users (Requires admin priveleges)
 set /p sinput=
 if %sinput%==1 move "Autorun.cmd" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 if %sinput%==2 move "Autorun.cmd" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
